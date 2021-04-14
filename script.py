0c4fc6ee67845a8c42717cfa2eee3e5c3c7a9a07
MisanthropicDeity <vidhujoshi31@gmail.com>
2021-04-14 16:30:47 +0530

    logs generation

--- script.py
+++ script.py
@@ -2,25 +2,26 @@ import boto3
 import os
 import datetime
 def upload_files(path):
+    logs = open("logs.txt", "a")
+    logs.write(f"Starting Backup for path {path} at {datetime.datetime.now}")
     session = boto3.Session(
         aws_access_key_id='AKIA5DAV65HJUPYKB5MM',
         aws_secret_access_key='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'
     )
+    logs.write("Session created Successfully")
     s3 = session.resource('s3')
     bucket = s3.Bucket('backup-test-101803645')
-    print(bucket.name)
-    print(path)
-    print(os.walk(path))
-    for root in os.walk(path):
-        print(root)
+    logs.write("bucket {bucket} opened")
+    
     for subdir, dirs, files in os.walk(path):
-        print("in")
         for file in files:
             full_path = os.path.join(subdir, file)
-            print(full_path)
+            logs.write(f"opening {full path}")
+            # print(full_path)
             with open(full_path, 'rb') as data:
-                bucket.put_object(Key=full_path[len(path)+1:], Body=data)
-                print(full_path)
+                log = bucket.put_object(Key=full_path[len(path)+1:], Body=data)
+                logs.write(log)
+                print(log)
 
 
 
